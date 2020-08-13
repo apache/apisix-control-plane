be7e05e87be1f8313746288ac515a3aac7003c1f
kv <gxthrj@163.com>
2020-08-13 14:09:19 +0800

    feat: add Rule yaml tranformer (#8)

--- doc/yaml_struct.md
+++ doc/yaml_struct.md
@@ -1,3 +1,22 @@
+<!--
+#
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+-->
+
 ## Currently, 4 types are defined, namely
 
 1.Gateway
@@ -41,12 +60,12 @@ gateways:
 - foo-gw
 http:
 - route:
- - destination:
+  - destination:
      port: 28002
      host: foo-server
      subset: foo-v1
      weight: 10
- label：
+  label:
     app: foo
     version: v1
   match:
--- pkg/yml/trans_test.go
+++ pkg/yml/trans_test.go
@@ -1,3 +1,20 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package yml_test
 
 import (
@@ -10,9 +27,9 @@ import (
 
 var _ = Describe("Trans", func() {
 	Describe("trans to model", func() {
-		var b []byte
-		BeforeEach(func() {
-			b = []byte(`
+		Context("trans", func() {
+			It("trans to gateway no error", func() {
+				b := []byte(`
 kind: Gateway
 name: foo-gw
 servers:
@@ -24,19 +41,65 @@ servers:
    - "a.foo.com"
    - "b.foo.com"
 `)
-			fmt.Println("BeforeEach executed")
-		})
-		Context("trans", func() {
-			It("trans to gateway no error", func() {
 				y, err := yaml.YAMLToJSON(b)
 				fmt.Println(string(y))
 				ym := yml.Trans(y, b)
 				Expect(err).NotTo(HaveOccurred())
-				Expect(ym.Type()).To(Equal("Gateway"))
+				v := typeof(ym)
+				fmt.Println(v)
+				Expect(v).To(Equal("*yml.Gateway"))
 				g, ok := ym.(*yml.Gateway)
 				Expect(ok).To(Equal(true))
 				Expect(len(g.Servers[0].Hosts)).To(Equal(2))
 			})
+
+			It("trans to rule no error", func() {
+				b := []byte(`
+kind: Rule
+name: xxx-rules
+hosts:
+- "a.foo.com"
+gateways:
+- foo-gw
+http:
+- route:
+  - destination:
+     port: 28002
+     host: foo-server
+     subset: foo-v1
+     weight: 10
+  label:
+    app: foo
+    version: v1
+  match:
+  - headers:
+     product_id:
+       exact: v1
+- route:
+  - destination:
+       port: 28002
+       host: foo-server
+       subset: v2
+  label:
+    app: foo
+    version: v2
+`)
+				y, err := yaml.YAMLToJSON(b)
+				fmt.Println(string(y))
+				ym := yml.Trans(y, b)
+				Expect(err).NotTo(HaveOccurred())
+				v := typeof(ym)
+				fmt.Println(v)
+				Expect(v).To(Equal("*yml.Rule"))
+				r, ok := ym.(*yml.Rule)
+				Expect(ok).To(Equal(true))
+				Expect(r.Kind).To(Equal("Rule"))
+				Expect(r.Kind).To(Equal("Rule"))
+			})
 		})
 	})
 })
+
+func typeof(v interface{}) string {
+	return fmt.Sprintf("%T", v)
+}

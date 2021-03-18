0959a24a352df13093d01acc6503b0b5dba730f7
imjoey <majunjie@apache.org>
2021-03-18 09:17:12 +0800

    chore: fix go code styles
    Signed-off-by: imjoey <majunjie@apache.org>

--- pkg/yml/plugin.go
+++ pkg/yml/plugin.go
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
- 
+
 package yml
 
 import "github.com/apache/apisix-control-plane/pkg/mem"
